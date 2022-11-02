70b3c27bacd5885c8f6bb81b70e6e819d4eb3ce1
KETAN THOMBARE <ketan.thombare@gmail.com>
2022-11-02 23:43:30 +0530

    added aws terraform

--- awsinstance.tf
+++
@@ -1,7 +0,0 @@
-# Configure the AWS Provider
-provider "aws" {
-    version = "3.53.0"
-    access_key = "AKIAWE3QSX7HFRXY5X5A"
-    secret_key = "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"
-    region     = "us-east-2"
-}
