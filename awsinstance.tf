db6b337061ed006f5318b55ec004b6148daf9cc2
KETAN THOMBARE <ketan.thombare@gmail.com>
2022-11-03 00:02:50 +0530

    edited new

--- awsinstance.tf
+++ awsinstance.tf
@@ -1,7 +1,10 @@
-# Configure the AWS Provider
 provider "aws" {
     version = "3.53.0"
     access_key = "AKIAWE3QSX7HFRXY5X5A"
     secret_key = "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"
     region     = "us-east-2"
+}
+resourse "aws instance" "Myfirstinstance" {
+    ami           = "ami-0f2e255ec956ade7f"
+    instance_type = "t2.micro"
 }
