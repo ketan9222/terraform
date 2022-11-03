0d2b810ee360dfc4cba63a44660548e0f4bd9c9b
KETAN THOMBARE <ketan.thombare@gmail.com>
2022-11-03 11:01:50 +0530

    update instance

--- awsinstance.tf
+++ awsinstance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    version = "3.53.0"
-    access_key = "AKIAWE3QSX7HFRXY5X5A"
-    secret_key = "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"
-    region     = "us-east-2"
+    access_key = ""
+    secret_key = ""
+    region     = "ap-south-1"
 }
-resourse "aws instance" "Myfirstinstance" {
-    ami           = "ami-0f2e255ec956ade7f"
-    instance_type = "t2.micro"
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-05692172625678b4e"
+  instance_type = "t2.micro"
 }
