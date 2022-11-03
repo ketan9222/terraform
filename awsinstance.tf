c8b7e111ee76f6c6f3bd9f9cefbae1e6c5cd8195
KETAN THOMBARE <ketan.thombare@gmail.com>
2022-11-03 12:34:10 +0530

    create seperate provider file

--- awsinstance.tf
+++ awsinstance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region     = "ap-south-1"
-}
-
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-05692172625678b4e"
   instance_type = "t2.micro"
